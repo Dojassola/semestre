ead76a39a2fd1e55ba78f240a55b27e19d64c163
Dojassola <59090344+Dojassola@users.noreply.github.com>
2024-05-13 19:57:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let num = []
 let div = document.getElementById("uga")
 for(let i=0;i<50;i++){
-    let a = Math.ceil(Math.random()* (100 - (-100) + 1)) -100 
+    let a = Math.ceil(Math.random() * (100 - (-100) + 1)) -100
+    a = (a>0) ? a : 0
     num.push(a)
     div.innerHTML += a+","
 }
